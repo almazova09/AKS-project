de8e30d81c11ef27e1a80991be759a7997170196
almazova09 <almazova1509@gmail.com>
2025-10-30 02:38:31 +0000

    made changes

--- main.tf
+++ main.tf
@@ -1,129 +1,24 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.21.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  use_cli = true
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "aks-resources"
-  location = "westus2"
-}
-
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "aks-network"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "public_subnet" {
-  name                 = "public-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-}
-
-resource "azurerm_subnet" "private_subnet" {
-  name                 = "private-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "aks-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_nat_gateway" "nat" {
-  name                = "aks-nat-gateway"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "Standard"
-}
-
-resource "azurerm_nat_gateway_public_ip_association" "nat_pip" {
-  nat_gateway_id       = azurerm_nat_gateway.nat.id
-  public_ip_address_id = azurerm_public_ip.public_ip.id
-}
-
-resource "azurerm_subnet_nat_gateway_association" "private_nat" {
-  subnet_id       = azurerm_subnet.private_subnet.id
-  nat_gateway_id = azurerm_nat_gateway.nat.id
-}
-
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "aks-security-group"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "allow-ssh"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "allow-http"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-resource "azurerm_subnet_network_security_group_association" "nsg_association" {
-  subnet_id                 = azurerm_subnet.public_subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "my-aks-cluster"
+  name                = var.aks_cluster_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "aksdemo"
 
   default_node_pool {
-    name            = "systempool"
-    node_count      = 1
-    vm_size         = "Standard_D2s_v3"
-    vnet_subnet_id  = azurerm_subnet.public_subnet.id
-    type            = "VirtualMachineScaleSets"
+    name                       = "publicpool"
+    node_count                 = 1
+    vm_size                    = var.node_vm_size
+    vnet_subnet_id             = azurerm_subnet.public_subnet.id
+    type                       = "VirtualMachineScaleSets"
     temporary_name_for_rotation = "tmp1"
-
   }
 
   network_profile {
-    network_plugin = "azure"
-    network_policy = "azure"
-    outbound_type  = "loadBalancer"
-    service_cidr       = "10.200.0.0/16"         
-    dns_service_ip     = "10.200.0.10"
-
+    network_plugin   = "azure"
+    network_policy   = "azure"
+    outbound_type    = "loadBalancer"
+    service_cidr     = "10.200.0.0/16"
+    dns_service_ip   = "10.200.0.10"
   }
 
   private_cluster_enabled = false
@@ -136,8 +31,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_kubernetes_cluster_node_pool" "privatepool" {
   name                  = "privatepool"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_D2s_v3"
+  vm_size               = var.node_vm_size
   node_count            = 1
   vnet_subnet_id        = azurerm_subnet.private_subnet.id
-
 }
