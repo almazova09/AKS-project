996b1ad0f1fea3ceb1971b9daa207bd33e7ae61f
almazova09 <almazova1509@gmail.com>
2025-11-06 02:55:45 +0000

    added terraform folder

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.13"
@@ -9,6 +13,11 @@ terraform {
       version = "~> 2.33"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
+  use_cli = true
 }
 
 provider "kubernetes" {
@@ -27,50 +36,114 @@ resource "kubernetes_namespace" "apps" {
   }
 }
 
+
+data "azurerm_container_registry" "acr" {
+  name                = "myprivateregistry15"
+  resource_group_name = "aks-resources"
+}
+
+
+resource "kubernetes_secret" "acr_auth" {
+  metadata {
+    name      = "acr-auth"
+    namespace = kubernetes_namespace.apps.metadata[0].name
+  }
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${data.azurerm_container_registry.acr.login_server}" = {
+          username = data.azurerm_container_registry.acr.admin_username
+          password = data.azurerm_container_registry.acr.admin_password
+          auth     = base64encode("${data.azurerm_container_registry.acr.admin_username}:${data.azurerm_container_registry.acr.admin_password}")
+        }
+      }
+    })
+  }
+}
+
+
 resource "helm_release" "mysql" {
-  name       = "mysql"
-  namespace  = kubernetes_namespace.apps.metadata[0].name
-  chart      = "../charts/mysql"
+  name              = "mysql"
+  namespace         = kubernetes_namespace.apps.metadata[0].name
+  chart             = "../Helm/mysql"
   dependency_update = false
+  depends_on        = [kubernetes_secret.acr_auth]
 
   set {
     name  = "auth.rootPassword"
     value = "rootpass123"
   }
+
   set {
     name  = "auth.user"
     value = "kaizen"
   }
+
   set {
     name  = "auth.password"
     value = "Hello123!"
   }
+
   set {
     name  = "auth.database"
     value = "hello"
   }
 }
 
+
 resource "helm_release" "api" {
   name       = "api"
   namespace  = kubernetes_namespace.apps.metadata[0].name
-  chart      = "../charts/api"
-  depends_on = [helm_release.mysql]
+  chart      = "../Helm/api"
+  depends_on = [helm_release.mysql, kubernetes_secret.acr_auth]
 
-  set { name = "image.repository"; value = "myprivateregistry15.azurecr.io/api" }
-  set { name = "image.tag"; value = "v1" }
-  set { name = "config.DBHOST"; value = "mysql" }
-  set { name = "secret.DBUSER"; value = "kaizen" }
-  set { name = "secret.DBPASS"; value = "Hello123!" }
+  set {
+    name  = "image.repository"
+    value = "${data.azurerm_container_registry.acr.login_server}/api"
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v1"
+  }
+
+  set {
+    name  = "config.DBHOST"
+    value = "mysql"
+  }
+
+  set {
+    name  = "secret.DBUSER"
+    value = "kaizen"
+  }
+
+  set {
+    name  = "secret.DBPASS"
+    value = "Hello123!"
+  }
 }
+
 
 resource "helm_release" "web" {
   name       = "web"
   namespace  = kubernetes_namespace.apps.metadata[0].name
-  chart      = "../charts/web"
-  depends_on = [helm_release.api]
+  chart      = "../Helm/web"
+  depends_on = [helm_release.api, kubernetes_secret.acr_auth]
 
-  set { name = "image.repository"; value = "myprivateregistry15.azurecr.io/web" }
-  set { name = "image.tag"; value = "v1" }
-  set { name = "config.API_HOST"; value = "http://api.apps.svc.cluster.local:3001" }
+  set {
+    name  = "image.repository"
+    value = "${data.azurerm_container_registry.acr.login_server}/web"
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v1"
+  }
+
+  set {
+    name  = "config.API_HOST"
+    value = "http://api.apps.svc.cluster.local:3001"
+  }
 }
